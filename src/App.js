e3c416567c320914600245c3007b99897b46c921
Gabe Rodriguez <gabe@spotify.com>
2019-10-22 15:27:41 +0200

    Adding SVG example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import CanvasEditor from "./CanvasEditor";
 import Header from "./Header";
-import './App.css'
+import "./App.css";
+import SVGEditor from "./svgEditor";
 
 export default () => (
   <>
     <Header />
     <CanvasEditor />
+    <SVGEditor />
   </>
 );
