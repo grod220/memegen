60f108c0fb14b74c850027ec5093731ea7eb3af4
Gabe Rodriguez <gabe@spotify.com>
2019-10-23 21:37:08 +0200

    Refactoring and adding proper download ability

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import CanvasEditor from "./CanvasEditor";
 import Header from "./Header";
 import "./App.css";
 import SVGEditor from "./SVGEditor";
@@ -7,7 +6,6 @@ import SVGEditor from "./SVGEditor";
 export default () => (
   <>
     <Header />
-    <CanvasEditor />
     <SVGEditor />
   </>
 );
